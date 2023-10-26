9008c7738b4c69743540aae9840a734b3046f81c
abyantahta <abyantahta2002@gmail.com>
2023-10-26 13:00:16 +0700

    :feat header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito, Nunito_Sans } from 'next/font/google'
 import clsx from 'clsx'
 import './globals.css'
 import { createClient } from '@/prismicio'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 const nunito = Nunito({
   subsets : ['latin'],
@@ -20,13 +22,13 @@ export async function generateMetadata(): Promise<Metadata> {
 
   const client = createClient();
 
-  const page = await client.getSingle("settings")
+  const settings = await client.getSingle("settings")
 
   return {
-    title: page.data.site_title || "Flowrise Fallback",
-    description:  page.data.meta_description || "Flowrise is the relaxing app you do",
+    title: settings.data.site_title || "Flowrise Fallback",
+    description:  settings.data.meta_description || "Flowrise is the relaxing app you do",
     openGraph: {
-      images: [page.data.og_image.url || ""],
+      images: [settings.data.og_image.url || ""],
     },
   }
 }
@@ -39,9 +41,9 @@ export default function RootLayout({
   return (
     <html className={clsx(nunito.variable, nunitoSans.variable)} lang="en">
       <body >
-        <header>Header!</header>
-        {children}
-      <footer>Footer!</footer>
+        <Header/>
+          {children}
+        <Footer/>
       </body>
     </html>
   )
